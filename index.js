c337120ae2efb97fe2be3ef94e9f950fdf3502ba
westhof91 <88407013+westhof91@users.noreply.github.com>
2021-09-03 14:52:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
+};
+
+ 
+
+function ternaryCheckCity(city){
+  return (city === 'NYC'?'Ok, sounds good.' : 'No go.') 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default: 
+      return 'Bye.'
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
